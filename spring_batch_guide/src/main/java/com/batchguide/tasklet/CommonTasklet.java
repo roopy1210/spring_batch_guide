b4f0cf63a702b42b85a9505c11b5e5750bcf454a
pncrm <pncrm@DESKTOP-GKHI4QK>
2019-01-15 23:53:42 +0900

    TASKLET 예제

---
+++ spring_batch_guide/src/main/java/com/batchguide/tasklet/CommonTasklet.java
@@ -0,0 +1,39 @@
+package com.batchguide.tasklet;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class CommonTasklet implements Tasklet {
+	
+	@Autowired
+	@Qualifier("batchOracleSqlSessionTemplate")
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	private String sqlId;
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		sqlSessionTemplate.insert(sqlId);
+		
+		return null;
+	}
+
+	/**
+	 * @return the sqlId
+	 */
+	public String getSqlId() {
+		return sqlId;
+	}
+
+	/**
+	 * @param sqlId the sqlId to set
+	 */
+	public void setSqlId(String sqlId) {
+		this.sqlId = sqlId;
+	}
+}
